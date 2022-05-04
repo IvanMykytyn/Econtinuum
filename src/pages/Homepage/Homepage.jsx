3bc696e3e0a732991f24fee9a91ff6062ba533b1
Max <maksymtsarynskii@gmail.com>
2022-05-04 12:58:07 +0300

    created new header

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -6,6 +6,21 @@ const Homepage = () => {
 
     return (
         <>
+        <div className="header">
+            <div className="header_section">
+                <div className="header_item headerLogo">
+                    Eco project
+                </div>
+                <div className="header_section">
+                    <div className="header_item headerButton"><a href="#"> Tutorial </a></div>
+                    <div className="header_item headerButton"><a href="#"> Select activities </a></div>
+                    <div className="header_item headerButton"><a href="#"> Rating </a></div>
+                </div>
+            </div>
+            <div className="header_section">
+                <div className="header_item headerButton"><a href="#"> Sing up </a></div>
+            </div>
+        </div>
         </>
     )
 }
