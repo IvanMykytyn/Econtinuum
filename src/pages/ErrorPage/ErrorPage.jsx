be7eb645397163e43c748e3b3344321dd8c3abfb
VanekCheck <mykytynivan04@gmail.com>
2022-05-25 00:25:29 +0300

    created horrible error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-import './error-page.styles.scss'
+import "./error-page.styles.scss";
+import { FaGhost } from "react-icons/fa";
 
 const ErrorPage = () => {
+  return (
+    <div className="content-error-page">
+      <h1>
+        4
+        <span>
+          <FaGhost />
+        </span>
+        4
+      </h1>
+      <h2>Error: 404 page not found</h2>
+      <p>Sorry, the page you're looking for cannot be accessed</p>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <h1>This page does not exist</h1>
-        </div>
-    )
-}
-
-export default ErrorPage
+export default ErrorPage;
