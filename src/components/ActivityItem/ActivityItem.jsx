cd9b315e1522edd3c997491dd877cb1ab83fe2ef
VanekCheck <mukutunivan04@gmail.com>
2022-06-10 22:51:00 +0300

    added ProtectedRoute and protected some functions

--- src/components/ActivityItem/ActivityItem.jsx
+++ src/components/ActivityItem/ActivityItem.jsx
@@ -1,29 +1,40 @@
-import React from "react";
-import {connect} from "react-redux";
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { useNavigate } from 'react-router-dom'
 
 import './activity-item.styles.scss'
-import {openActivityForm} from "../../redux/activityForm/activityForm.actions";
+import { openActivityForm } from '../../redux/activityForm/activityForm.actions'
 
-const ActivityItem = ({id,activity,openForm}) => {
-    const {title,description,icon,number_of_points,unit_of_measure} = activity;
-    return(
-        <div className={`item__${id+1} item`} onClick={()=>openForm(activity)}>
-            <img src={`data:image/svg+xml;base64,${icon}`} alt=""/>
-            <div>
-            <h3>{title}</h3>
-            <p className="task-description">
-                {description}
-            </p>
-            <p className="count-of">
-                <span>{number_of_points}</span> points per {unit_of_measure}
-            </p>
-            </div>
-        </div>
-    )
+const ActivityItem = ({ id, activity, openForm }) => {
+  let navigate = useNavigate()
+
+  const { title, description, icon, number_of_points, unit_of_measure } =
+    activity
+
+  const handleClick = () => {
+    const user = localStorage.getItem('user')
+    if (user) {
+      return openForm(activity)
+    } else {
+      return navigate('/sign-in')
+    }
+  }
+  return (
+    <div className={`item__${id + 1} item`} onClick={handleClick}>
+      <img src={`data:image/svg+xml;base64,${icon}`} alt="" />
+      <div>
+        <h3>{title}</h3>
+        <p className="task-description">{description}</p>
+        <p className="count-of">
+          <span>{number_of_points}</span> points per {unit_of_measure}
+        </p>
+      </div>
+    </div>
+  )
 }
 const mapDispatchToProps = (dispatch) => ({
-    openForm: (item) => dispatch(openActivityForm(item))
+  openForm: (item) => dispatch(openActivityForm(item)),
 })
 
-
-export default connect(null,mapDispatchToProps)(ActivityItem)
+export default connect(null, mapDispatchToProps)(ActivityItem)
--- src/pages/Rating/Rating.jsx
+++ src/pages/Rating/Rating.jsx
@@ -1,39 +1,40 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import "./rating.styles.scss";
-import "../Profile/profile.styles.scss";
+import './rating.styles.scss'
+import '../Profile/profile.styles.scss'
 
+import { MdPersonOutline } from 'react-icons/md'
+import { BsDot } from 'react-icons/bs'
 
-import { MdPersonOutline } from "react-icons/md";
-import { BsDot } from "react-icons/bs";
-
-import Dashboard from "../../components/Dashboard/Dashboard";
-import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
-
+import Dashboard from '../../components/Dashboard/Dashboard'
+import CategoryFilter from '../../components/CategoryFilter/CategoryFilter'
 
 const Rating = () => {
-
-  const [showMe, setShowMe] = useState(false);
+  const [showMe, setShowMe] = useState(false)
 
   return (
     <div className="rating-content">
       <nav>
         <div className="filter-box">
           <ul>
-            <li className="show-me-section">
-              <div
-                className={"show-me-title" + (showMe ? " active-show-me" : "")}
-                onClick={() => setShowMe(!showMe)}
-              >
-                <div>
-                  <MdPersonOutline size={30} />
-                  <h4>Show me</h4>
+            {localStorage.getItem('user') && (
+              <li className="show-me-section">
+                <div
+                  className={
+                    'show-me-title' + (showMe ? ' active-show-me' : '')
+                  }
+                  onClick={() => setShowMe(!showMe)}
+                >
+                  <div>
+                    <MdPersonOutline size={30} />
+                    <h4>Show me</h4>
+                  </div>
+                  <BsDot size={30} />
                 </div>
-                <BsDot size={30} />
-              </div>
-            </li>
+              </li>
+            )}
 
-            <CategoryFilter/>
+            <CategoryFilter />
           </ul>
         </div>
       </nav>
@@ -42,7 +43,7 @@ const Rating = () => {
         <Dashboard />
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default Rating;
+export default Rating
