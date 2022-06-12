681dc9ae693e980c4ccd2ef23e227763f655de52
VanekCheck <mukutunivan04@gmail.com>
2022-06-12 14:46:01 +0300

    fix some bugs and add .env variable

--- src/pages/SIgnIn/SignIn.jsx
+++ src/pages/SIgnIn/SignIn.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import "./sign-in.styles.scss";
-import FormInput from "../../components/FormInput/FormInput";
-import Password from "../../components/Password/Password";
-import CustomButton from "../../components/CustomButton/CustomButton";
+import './sign-in.styles.scss'
+import FormInput from '../../components/FormInput/FormInput'
+import Password from '../../components/Password/Password'
+import CustomButton from '../../components/CustomButton/CustomButton'
 
-import { Link, useNavigate } from "react-router-dom";
-import TitleFormField from "../../components/_common/TitleFormField/TitleFormField";
-import SubtitleFormField from "../../components/_common/SubtitleFormField/SubtitleFormField";
-import { validateInput } from "../../utils/SignIn/validateInput";
+import { Link, useNavigate } from 'react-router-dom'
+import TitleFormField from '../../components/_common/TitleFormField/TitleFormField'
+import SubtitleFormField from '../../components/_common/SubtitleFormField/SubtitleFormField'
+import { validateInput } from '../../utils/SignIn/validateInput'
 import {
   authFailureReset,
   userSignInRequest,
   userSignRequestViaGoogle,
-} from "../../redux/auth/auth.actions";
-import { connect } from "react-redux";
-import axios from "axios";
-import { GoogleLogin } from "react-google-login";
+} from '../../redux/auth/auth.actions'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import { GoogleLogin } from 'react-google-login'
 
 const SignIn = ({
   signInRequest,
@@ -25,61 +25,60 @@ const SignIn = ({
 
   auth: { isLoading, errorMessage },
 }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   useEffect(
     () => () => {
       if (errorMessage) {
-        resetErrorMessage();
+        resetErrorMessage()
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
     },
     []
-  );
+  )
   const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const [errors, setErrors] = useState({});
+    email: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState({})
   const handleChange = ({ target: { name, value } }) => {
-    setUser({ ...user, [name]: value });
-    setErrors({ ...errors, [name]: "" });
-  };
+    setUser({ ...user, [name]: value })
+    setErrors({ ...errors, [name]: '' })
+  }
   const isValid = () => {
-    const { errors, isValid } = validateInput(user);
+    const { errors, isValid } = validateInput(user)
     if (!isValid) {
-      setErrors(errors);
+      setErrors(errors)
     }
 
-    return isValid;
-  };
+    return isValid
+  }
 
   const handleOAuth = async (googleData) => {
     try {
-      const result = await signRequestViaGoogle(googleData);
+      const result = await signRequestViaGoogle(googleData)
       if (result) {
-        setErrors({});
-        navigate("/");
+        setErrors({})
+        navigate('/')
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
     if (isValid()) {
-      const result = await signInRequest(user);
-      console.log(result);
+      const result = await signInRequest(user)
       if (result) {
         setUser({
-          email: "",
-          password: "",
-        });
-        setErrors({});
-        navigate("/");
+          email: '',
+          password: '',
+        })
+        setErrors({})
+        navigate('/')
       }
     }
-  };
+  }
   return (
     <div className="sign-in-wrapper">
       <div className="sign-form">
@@ -87,24 +86,24 @@ const SignIn = ({
         <SubtitleFormField>Sign in to your account</SubtitleFormField>
         <form onSubmit={handleSubmit}>
           <FormInput
-            name={"email"}
+            name={'email'}
             type="text"
-            label={"Email address"}
+            label={'Email address'}
             value={user.email}
             onChange={handleChange}
             error={errors.email}
           />
           <Password
-            name={"password"}
-            label={"Password"}
+            name={'password'}
+            label={'Password'}
             value={user.password}
             onChange={handleChange}
             error={errors.password ? errors.password : errorMessage}
           />
 
           <div className="sign-form__link">
-            Forgot your{" "}
-            <Link className="sign-form__link-forget" to={"/forget"}>
+            Forgot your{' '}
+            <Link className="sign-form__link-forget" to={'/forget'}>
               password?
             </Link>
           </div>
@@ -114,7 +113,7 @@ const SignIn = ({
           </CustomButton>
           <p className="sign-in__text-or">or</p>
           <GoogleLogin
-            clientId={process.env.GOOGLE_CLIENT_ID}
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
             render={(renderProps) => (
               <CustomButton
                 googleButton
@@ -130,22 +129,22 @@ const SignIn = ({
         </form>
 
         <div className="sign-form__link">
-          Don't have an account?{" "}
+          Don't have an account?{' '}
           <Link className="sign-form__link-not" to="/sign-up">
             Sign up
           </Link>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const mapDispatchToProps = (dispatch) => ({
   signInRequest: (data) => dispatch(userSignInRequest(data)),
   signRequestViaGoogle: (user) => dispatch(userSignRequestViaGoogle(user)),
   resetErrorMessage: () => dispatch(authFailureReset()),
-});
+})
 const mapStateToProps = (state) => ({
   auth: state.auth,
-});
-export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
+})
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
